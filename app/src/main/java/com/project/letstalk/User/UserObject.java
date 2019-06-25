e3837ebecbe9438049272a1a368a1cca42922521
Radhika Agarwal <radhikaagg99@gmail.com>
2019-06-25 03:12:14 +0000

    Initial commit

---
+++ app/src/main/java/com/project/letstalk/User/UserObject.java
@@ -0,0 +1,27 @@
+package com.project.letstalk.User;
+
+public class UserObject {
+
+    private String name, phone, uid;
+
+    public UserObject(String uid, String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+        this.uid = uid;
+    }
+
+    public String getUid() { return uid; }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
