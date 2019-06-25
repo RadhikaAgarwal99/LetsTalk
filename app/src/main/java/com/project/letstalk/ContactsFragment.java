1ad20e47773df5680ce881c34a4e60bf60d6ef20
Radhika_Agarwal <radhikaagg99@gmail.com>
2019-06-25 15:47:07 +0530

    Adding country code picker and updating the GUI of the sign up page

---
+++ app/src/main/java/com/project/letstalk/ContactsFragment.java
@@ -0,0 +1,39 @@
+package com.project.letstalk;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ContactsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ContactsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ContactsFragment extends Fragment {
+
+    public ContactsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/project/letstalk/GroupsFragment.java
@@ -0,0 +1,36 @@
+package com.project.letstalk;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GroupsFragment extends Fragment {
+
+
+    public GroupsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_groups, container, false);
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
